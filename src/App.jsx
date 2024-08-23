275c62560667c71d61c6ad1179929baa26790a79
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-23 14:01:17 +0300

    Add properties to state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,30 @@ import "./App.css";
 import { PlayerControls } from "./components/PlayerControls";
 import { TrackList } from "./components/TrackList";
 import { MusicContext } from "./contexts/MusicContext";
+import sinister from "./assets/sinister.mp3";
+import stomp from "./assets/stomp.mp3";
+import underwater from "./assets/underwater.mp3";
 
 function App() {
-  const [state, setState] = useState();
+  const [state, setState] = useState({
+    audioPlayer: new Audio(),
+    currentTrackIndex: 0,
+    isPlaying: false,
+    tracks: [
+      {
+        name: "Sinister",
+        file: sinister,
+      },
+      {
+        name: "Stomp",
+        file: stomp,
+      },
+      {
+        name: "Underwater",
+        file: underwater,
+      },
+    ],
+  });
   return (
     <MusicContext.Provider value={[state, setState]}>
       <div>
